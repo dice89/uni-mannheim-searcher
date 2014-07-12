459ab9e4f40407d47378238e68d7aa73ada14ca8
Michael Dell <apfelbaum24@googlemail.com>
2014-07-12 13:44:30 +0200

    new crawl

--- unimaPeopleSearch/src/de/unima/peoplesearch/extraction/ParallelPipeline.java
+++ unimaPeopleSearch/src/de/unima/peoplesearch/extraction/ParallelPipeline.java
@@ -55,7 +55,7 @@ public class ParallelPipeline extends Pipeline {
 	}
 	
 	public static void main(String args[]){
-		ParallelPipeline pp = new ParallelPipeline(Integer.MAX_VALUE, 50);
+		ParallelPipeline pp = new ParallelPipeline(Integer.MAX_VALUE, 33);
 		try {
 			pp.startExtraction();
 		} catch (IOException e) {
